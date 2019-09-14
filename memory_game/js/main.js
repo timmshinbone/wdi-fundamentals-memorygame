974843ba758f4fd11237f13d0c710e792350482e
Timm Schoenborn <tmmschnbrn@Timms-MacBook-Air.local>
2019-09-14 00:38:38 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,6 +1,23 @@
-var cardOne = "queen";
-var cardTwo = "king";
-var cardThree = "queen";
-var cardFour = "king";
+
+const cards = ["Queen", "Queen", "King", "King"]
+
+var cardOne = cards[0];
+var cardTwo = cards[2];
+
+const cardsInPlay = [];
+
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo);
+
+
 console.log("User flipped " + cardOne);
 console.log("User flipped " + cardTwo);
+
+
+if (cardsInPlay.length === 2) {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+	alert("You found a match!");
+}	else { 
+	alert("Sorry, Try again!");
+}
+}
