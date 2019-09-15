bc9c1802650b76eb4891285585674c342279ba1b
Timm Schoenborn <tmmschnbrn@Timms-MacBook-Air.local>
2019-09-15 00:35:23 -0500

    converted arrays to objects

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,20 +1,43 @@
 
-var cards = ["Queen", "Queen", "King", "King"]
+var cards = [
+	{
+		rank: "Queen",
+		suit: "hearts",
+		cardImage: "images/queen-of-hearts.png"
+	} ,
+	{
+		rank: "Queen",
+		suit: "diamonds",
+		cardImage: "images/queen-of-diamonds.png"
+	} ,
+	{
+		rank: "King",
+		suit: "hearts",
+		cardImage: "images/king-of-hearts.png"
+	} ,
+	{
+		rank: "King",
+		suit: "diamonds",
+		cardImage: "images/king-of-diamonds.png"
+	}
+	];
 
 
 var cardsInPlay = [];
 
 function checkForMatch() {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
-	console.log("You found a match!");
+	alert("You found a match!");
 	} else { 
-	console.log("Sorry, Try again!");
+	alert("Sorry, Try again!");
 	}
 }
 	
-	function flipCard(cardID) {
-		console.log("User flipped " + cards[cardID]);
-		cardsInPlay.push(cards[cardID]);
+	function flipCard(cardId) {
+		console.log("User flipped " + cards[cardId].rank);
+		cardsInPlay.push(cards[cardId].rank);
+		console.log(cards[cardId].cardImage);
+		console.log(cards[cardId].suit);
 
 		if (cards.length === 2) {
 		checkForMatch();
