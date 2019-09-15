02b1d56bb88cfbe71bcc61d1c3e43c3074140454
Timm Schoenborn <tmmschnbrn@Timms-MacBook-Air.local>
2019-09-15 18:11:39 -0500

    finalized memory game

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -25,26 +25,35 @@ var cards = [
 
 var cardsInPlay = [];
 
+function createBoard() {
+	for (var i = 0; i < cards.length; i++) {
+	var cardElement = document.createElement('img');
+	cardElement.setAttribute('src', "images/back.png");
+	cardElement.setAttribute('data-id', i);
+	cardElement.addEventListener('click', flipCard);
+	document.getElementById('game-board').appendChild(cardElement);
+	};
+};
+
+function flipCard() {
+	var cardId = this.getAttribute('data-id');
+	console.log("User flipped " + cards[cardId].rank);
+	console.log(cards[cardId].cardImage);
+	console.log(cards[cardId].suit);
+	cardsInPlay.push(cards[cardId].rank);
+	this.setAttribute('src', cards[cardId].cardImage);
+	if (cardsInPlay.length === 2) {
+	checkForMatch();
+	}
+};
+
 function checkForMatch() {
-	if (cardsInPlay[0] === cardsInPlay[1]) {
-	alert("You found a match!");
-	} else { 
-	alert("Sorry, Try again!");
-	}
-}
-	
-	function flipCard(cardId) {
-		console.log("User flipped " + cards[cardId].rank);
-		cardsInPlay.push(cards[cardId].rank);
-		console.log(cards[cardId].cardImage);
-		console.log(cards[cardId].suit);
-
-		if (cards.length === 2) {
-		checkForMatch();
+		if (cardsInPlay[0] === cardsInPlay[1]) {
+			alert("You found a match!");
+		} else { 
+			alert("Sorry, Try again!");
 		}
-	}
+};
+	
 
-flipCard(0);
-flipCard(2);
-
-checkForMatch();
+createBoard();
